3a8b4b20def05b163cf35dc836841857b3d42fc4
SIVASANGARANKK <sivasucbe@gmail.com>
2026-01-16 11:46:14 +0530

    mvn-jen-demo

---
+++ indiaproj/Dockerfile
@@ -0,0 +1,7 @@
+FROM openjdk:17.0.2-jdk
+ 
+WORKDIR /app
+ 
+COPY target/indiaproj-1.0.jar /app/DemoMaven.jar
+ 
+ENTRYPOINT ["java","-jar","/app/DemoMaven.jar"]
---
+++ indiaproj/src/main/java/com/wip/DemoMaven.java
@@ -0,0 +1,7 @@
+package com.wip;
+
+public class DemoMaven {
+	public static void main(String[] args) {
+		System.out.println("Maven Java within docker");
+	}
+}
